f8d040f4ee85a490497f710d1ddc05aa18ef7c90
Armazok <nikolai.borsin@mail.ru>
2022-08-17 21:12:40 +0500

    Updates

---
+++ precache-manifest.d76e46959b90cec8957b86daddbd4ebd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c046ec71148700998e25af0f4bc0d670",
+    "url": "/HWIGnat/index.html"
+  },
+  {
+    "revision": "6a29b439c6ce2bf03a82",
+    "url": "/HWIGnat/static/css/main.ead3ced2.chunk.css"
+  },
+  {
+    "revision": "1ea0684cc51ba998e05f",
+    "url": "/HWIGnat/static/js/2.bff5e6d1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/HWIGnat/static/js/2.bff5e6d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a29b439c6ce2bf03a82",
+    "url": "/HWIGnat/static/js/main.380c32af.chunk.js"
+  },
+  {
+    "revision": "58d0cf4d25384392bdae",
+    "url": "/HWIGnat/static/js/runtime-main.e03fa613.js"
+  }
+]);
